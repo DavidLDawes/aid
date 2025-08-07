bd246a9abe3ae2b4f81848022e78e23d9ba02c08
David Dawes <davidlymandawes@yahoo.com>
2025-08-07 12:44:30 -0700

    Remove stuff I no longer use (#86)

--- backend/add_missile_reloads.sql
+++
@@ -1,9 +0,0 @@
--- Add missile_reloads column to ships table
--- This column stores the tonnage of missile reloads allocated for the ship
-
-ALTER TABLE ships 
-ADD COLUMN missile_reloads INT NOT NULL DEFAULT 0 
-COMMENT 'Tonnage of missile reloads (1 MCr per ton)';
-
--- Verify the column was added
-DESCRIBE ships;
